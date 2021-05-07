4686629a63598dd746cc92b316ad51d9f4eb6767
EJ <ej.roux13@gmail.com>
2021-05-06 18:18:55 -0700

    fixed typo when when calling mongoose model

--- models/Workout.js
+++ models/Workout.js
@@ -41,6 +41,6 @@ const workoutSchema = new Schema({
     ]
 });
 
-const Workout = mongoose.Model("Workout", workoutSchema);
+const Workout = mongoose.model("Workout", workoutSchema);
 
 module.exports = Workout;

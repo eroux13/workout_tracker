917335d50ec21abfccbd46dead653d6d0102b3d8
EJ <ej.roux13@gmail.com>
2021-05-08 23:53:35 -0700

    rearranged base routes since the other routes were not triggering and throwing errors

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,16 +3,16 @@ const router = require("express").Router();
 const path = require("path");
 
 // Routes
-router.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-});
-
 router.get("/exercise", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
 
 router.get("/stats", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+
+router.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 const apiRoutes = require("./api");
 const htmlRoutes = require("./htmlRoutes");
 
-router.use("/", htmlRoutes);
 router.use("/api", apiRoutes);
+router.use("/", htmlRoutes);
 
 module.exports = router;
